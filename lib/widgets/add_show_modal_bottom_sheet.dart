9323ebc4b35a0783bd036412d8562904ec7d7b13
Mohamed Atta <mohamedattamersal2001@gmail.com>
2024-10-06 15:57:58 +0300

    add add  BlocConsumer and ModalProgressHUD

--- lib/widgets/add_show_modal_bottom_sheet.dart
+++ lib/widgets/add_show_modal_bottom_sheet.dart
@@ -1,18 +1,37 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_state.dart';
 import 'package:notes_app/widgets/add_notes_form.dart';
-
 
 class AddShowModalBottomSheet extends StatelessWidget {
   const AddShowModalBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: SingleChildScrollView(
-        child: AddNotesForm(),
-      ),
+    return BlocConsumer<AddNoteCubit, AddNoteState>(
+      listener: (context, state) {
+        if (state is AddNoteFailure) {
+          log("Failure");
+        }
+
+        if (state is AddNoteSuccess) {
+          Navigator.pop(context);
+        }
+      },
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: SingleChildScrollView(
+            child: ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: const AddNotesForm(),
+            ),
+          ),
+        );
+      },
     );
   }
 }
-
